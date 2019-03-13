c1f9a0d6a181e84992a1cc0d724cd52e87f7312d
Vasu Goel <goelvasu0712@gmail.com>
2019-03-13 12:44:48 +0000

    Add app config

--- website/app.js
+++ website/app.js
@@ -1,4 +1,8 @@
 const express = require("express"),
       app = express(),
       bodyParser = require("body-parser");
-      
+
+// App Config
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
