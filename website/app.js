8d748f609f744a93cfb10c222368f8b67c76ff89
Vasu Goel <goelvasu0712@gmail.com>
2019-04-17 21:14:23 +0000

    Create transporter object, setup email, send email with defined object

--- website/app.js
+++ website/app.js
@@ -4,7 +4,8 @@ const express = require("express"),
       bodyParser = require("body-parser"),
       User = require("./models/user"),
       LocalStrategy = require("passport-local"),
-      passportLocalMongoose = require("passport-local-mongoose");
+      passportLocalMongoose = require("passport-local-mongoose"),
+      nodemailer = require('nodemailer');
       
 // Mongoose config
 mongoose.connect("mongodb://localhost/auth", {useNewUrlParser: true});
@@ -14,6 +15,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.use(require("express-session")({
     secret: "Vasu is actually pretty frickin funny",
@@ -115,7 +117,62 @@ function isLoggedIn(req, res, next) {
 }
 
 
-// Contact fomr submission
+// Contact form submission route
+app.post("/contact", (req, res) => {
+      const name = req.body.cf_name;
+    const output = `
+        <p>Hello,<p>
+        <p>You have a message from, ${req.body.cf_name}</p>
+        <h3>Contact Details</h3>
+        <ul>  
+            <li>Name: ${req.body.cf_name}</li>
+            <li>Email: ${req.body.cf_email}</li>
+        </ul>
+        <h3>Message:</h3>
+        <p>${req.body.cf_message}</p>
+    `
+    
+// create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: 'true',
+        auth: {
+            user: 'node.mailer.smtp@gmail.com', // could use generated ethereal user
+            pass: 'nodemailertest123'  // could use generated ethereal password
+        },
+        tls:{
+          rejectUnauthorized:false
+        }
+    });
+      
+// setup email data with unicode symbols
+    let mailOptions = {
+      from: `${name} <node.mailer.smtp@gmail.com>`, // sender address
+      to: 'uautomate.com@gmail.com', // list of receivers
+      subject: 'üAutomate Customer Feedback', // Subject line
+      text: 'Hello world?', // plain text body
+      html: output // html body
+    };
+
+// send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+              return console.log(error);
+          }
+          console.log('Message sent: %s', info.messageId);   
+          console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    
+          res.redirect('/contact/send');
+    });
+});
+
+
+// render thank you page upon successful form submission
+app.get("/contact/send", (req, res) => {
+      res.render("messagesent");   
+});
 
 
 // Starts a UNIX socket and listens for connections on the given path
